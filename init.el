1f4d748e85f0a5beb12f7239446e4f8009c5c322
Eivind Hjertnes <me@hjertnes.me>
2018-11-07 19:05:00 +0100

    update

--- init.el
+++ init.el
@@ -30,17 +30,3 @@
       (funcall initial-major-mode)
       (setq buffer-offer-save t)
       $buf))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (elisp-format cider taskpaper-mode markdown-mode lsp-python magit deft rainbow-identifiers rainbow-delimiters which-key indent-guide ace-jump-mode lsp-mode company flycheck counsel-projectile counsel projectile exec-path-from-shell smex smartparens use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
