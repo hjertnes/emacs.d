aaa9b96a33852578d7289156fed7db58bde03cf0
Eivind Hjertnes <me@hjertnes.me>
2018-11-15 04:53:18 +0100

    update

---
+++ csharp2.el
@@ -0,0 +1,18 @@
+;; Copied from the omnisharp github page. Adds support for Csharp auto completion.
+;; Set the omnisharp server path manually on windows, because that piece of shit is weird, and only work this way.
+
+(use-package 
+  omnisharp 
+  :ensure t 
+  (when (is-windows) 
+    :init (setq omnisharp-server-executable-path "C:\\Bin\\omnisharp-roslyn\\OmniSharp.exe")) 
+  :config (progn (add-to-list 'company-backends #'company-omnisharp) 
+		 (add-hook 'csharp-mode-hook 
+			   (lambda() 
+			     (omnisharp-mode) 
+			     (company-mode) 
+			     (flycheck-mode) 
+			     (setq indent-tabs-mode nil c-syntactic-indentation t c-basic-offset 4
+				   truncate-lines t tab-width 4) 
+			     (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring) 
+			     (local-set-key (kbd "C-c C-c") 'recompile)))))
