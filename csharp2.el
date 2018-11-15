59d96a5927c1abb8f22da28cdb815395e2879469
Eivind Hjertnes <me@hjertnes.me>
2018-11-15 07:07:26 +0100

    Update csharp2.el

--- csharp2.el
+++ csharp2.el
@@ -1,10 +1,9 @@
-;; Copied from the omnisharp github page. Adds support for Csharp auto completion.
+;; Code for auto completion etc with .NET
 ;; Set the omnisharp server path manually on windows, because that piece of shit is weird, and only work this way.
-
-(use-package 
-  omnisharp 
-  :ensure t 
-  (when (is-windows) 
+(when (is-windows)
+  (use-package 
+    omnisharp 
+    :ensure t 
     :init (setq omnisharp-server-executable-path "C:\\Bin\\omnisharp-roslyn\\OmniSharp.exe")) 
   :config (progn (add-to-list 'company-backends #'company-omnisharp) 
 		 (add-hook 'csharp-mode-hook 
