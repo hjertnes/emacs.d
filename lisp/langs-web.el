e96a04f871d7bf498115821ab92606d8154d8546
Eivind Hjertnes <me@hjertnes.me>
2018-12-02 18:59:00 +0100

    update

--- lisp/langs-web.el
+++ lisp/langs-web.el
@@ -6,13 +6,4 @@
 (use-package 
   rjsx-mode 
   :ensure t)
-;; LSP server for JavaScript
-;; remember: npm i -g javascript-typescript-langserver
-(use-package 
-  lsp-javascript-typescript 
-  :ensure t
-  :config (progn (add-hook 'js-mode-hook #'lsp-javascript-typescript-enable)
-		 (add-hook 'typescript-mode-hook #'lsp-javascript-typescript-enable)
-		 (add-hook 'js3-mode-hook #'lsp-javascript-typescript-enable)
-		 (add-hook 'rjsx-mode #'lsp-javascript-typescript-enable)))
 (provide 'langs-web)
