dde7035c65b47b1cf170d8d56923d73d80200a43
Eivind Hjertnes <me@hjertnes.me>
2019-01-15 19:39:06 +0100

    Some more js setup, to see if it makes things better at work

--- lisp/langs-web.el
+++ lisp/langs-web.el
@@ -2,8 +2,16 @@
 (use-package 
   web-mode 
   :ensure)
+(use-package 
+  json-mode 
+  :ensure)
+(use-package 
+  js2-mode 
+  :ensure)
+
 ;; React support
 (use-package 
   rjsx-mode 
   :ensure t)
+(use-package langs-js :after (web-mode json-mode js2-mode flycheck))
 (provide 'langs-web)
