77da02627e12c051a813ee480f097d9a5c4021eb
Eivind Hjertnes <me@hjertnes.me>
2019-02-05 13:55:48 +0100

    Update core.el

--- lisp/core.el
+++ lisp/core.el
@@ -33,11 +33,32 @@
   (progn
     (require 'smartparens-config)
     (smartparens-global-strict-mode +1)
-    (show-smartparens-global-mode +1)))
+    (show-smartparens-global-mode +1))
+:diminish smartparens-mode
+:bind (("C-M-k" . sp-kill-sexp-with-a-twist-of-lime)
+       ("C-M-f" . sp-forward-sexp)
+       ("C-M-b" . sp-backward-sexp)
+       ("C-M-n" . sp-up-sexp)
+       ("C-M-d" . sp-down-sexp)
+       ("C-M-u" . sp-backward-up-sexp)
+       ("C-M-p" . sp-backward-down-sexp)
+       ("C-M-w" . sp-copy-sexp)
+       ("M-s" . sp-splice-sexp)
+       ("M-r" . sp-splice-sexp-killing-around)
+       ("C-)" . sp-forward-slurp-sexp)
+       ("C-}" . sp-forward-barf-sexp)
+       ("C-(" . sp-backward-slurp-sexp)
+       ("C-{" . sp-backward-barf-sexp)
+       ("M-S" . sp-split-sexp)
+       ("M-J" . sp-join-sexp)
+       ("C-M-t" . sp-transpose-sexp))
+  )
+
 ;; Loading UI stuff
 (require 'core-ui)
 ;; Loading Ivy/Counsel
 (require 'core-ivy)
+
 ;; Loading keyboard related modules
 (require 'core-keyboard)
 (provide 'core)
