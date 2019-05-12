37e0082869f4b838b2c1c9ed054224e0bce73b87
Eivind Hjertnes <me@hjertnes.me>
2019-05-12 16:04:43 +0200

    update

--- lisp/langs.el
+++ lisp/langs.el
@@ -95,5 +95,6 @@
 			       (cider-load-buffer))))))
 (use-package 
   org :ensure t :pin org)
-;(use-package org-plus-contrib :ensure t :pin org)
+(use-package org-bullets :ensure t :config (add-hook 'org-mode-hook 'org-bullets-mode))
+(use-package org-beautify-theme :ensure t)
 (provide 'langs)
