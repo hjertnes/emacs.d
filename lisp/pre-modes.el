485d2107e1e41d4e1f90a22d30a505e27d8f7c6f
Eivind Hjertnes <me@hjertnes.me>
2019-05-13 18:03:05 +0200

    update

--- lisp/pre-modes.el
+++ lisp/pre-modes.el
@@ -2,8 +2,6 @@
 (global-auto-revert-mode +1)
 ;; Auto save
 (auto-save-visited-mode +1)
-;; line numbers
-(global-linum-mode +1)
 ;; Dont show scrollbars or those ugly ass icons
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
@@ -12,12 +10,10 @@
 ;; Matching deliminers
 (electric-pair-mode +1)	;disabled to see if I prefer to do it manually
 ;; Highlight the current line
-					; Only highligt current line when in GUI mode, because some lines became unreadable in termianl
+;;highligt current line when in GUI mode, because some lines became unreadable in termianl
 (global-hl-line-mode 1)
 ;; Wrap lines
 (global-visual-line-mode 1)
-;; Pretty symbols
-(global-prettify-symbols-mode 1)
 ;; Show matching parens
 (show-paren-mode 1)
 ;; C-z are set to work more like most undo systems would
