192323c0d3c39d0dd26e2651ab48e4e4ce30abf0
Eivind Hjertnes <me@hjertnes.me>
2019-08-23 11:50:37 +0200

    Update

--- init.el
+++ init.el
@@ -1,11 +1,3 @@
-(when(not(string-equal system-type "windows-nt")) 
-  (progn
-    (when (not(file-exists-p "~/.emacs.d/personal.el")) 
-      (comint-send-string (get-buffer-process (shell)) "touch ~/.emacs.d/personal.el"))
-
-    (when (not(file-exists-p "~/.emacs.d/custom.el")) 
-      (comint-send-string (get-buffer-process (shell)) "touch ~/.emacs.d/custom.el"))))
-
 (require 'org)
 (org-babel-load-file "~/.emacs.d/hjertnes.org")
 
