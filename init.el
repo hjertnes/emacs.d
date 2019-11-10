523fb6c3dabcf6fc973877e2102876f2b916e10a
Eivind Hjertnes <me@hjertnes.me>
2019-11-10 09:49:46 +0100

    Cleaned up init.el

--- init.el
+++ init.el
@@ -1,6 +1,10 @@
+;; Load configuration from Org Document
 (require 'org)
 (org-babel-load-file "~/.emacs.d/hjertnes.org")
 
+;; Place all the custom junk in a custom.el file
+(load (expand-file-name "custom.el" user-emacs-directory))
+
+;; Per computer overrides
 (load "~/.emacs.d/personal.el")
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
+
