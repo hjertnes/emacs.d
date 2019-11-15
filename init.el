4b8798f708bbed5316ccecb2040ef60119ebf09e
Eivind Hjertnes <me@hjertnes.me>
2019-11-15 10:14:27 +0100

    Fix the custom crap

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
-;; Load configuration from Org Document
-(require 'org)
-(org-babel-load-file "~/.emacs.d/hjertnes.org")
-
 ;; Place all the custom junk in a custom.el file
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+
+;; Load configuration from Org Document
+(require 'org)
+(org-babel-load-file "~/.emacs.d/hjertnes.org")
 
 ;; Per computer overrides
 (load "~/.emacs.d/personal.el")
