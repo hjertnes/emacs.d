511093b0318c8ceeb16bc287bfd8410f654f1d90
Eivind Hjertnes <me@hjertnes.me>
2020-07-02 17:51:42 +0200

    Started using create-empty-file instead of crashing when files are missing

--- init.el
+++ init.el
@@ -1,4 +1,10 @@
 ;; Place All The Custom junk in a custom.el file
+(defun create-if-not-exist(filename)
+  (if (not(file-exists-p filename))
+      (make-empty-file filename)))
+
+
+(create-if-not-exist "~/.emacs.d/custom.el")
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
@@ -7,4 +13,5 @@
 (org-babel-load-file "~/.emacs.d/hjertnes.org")
 
 ;; Per computer overrides
+(create-if-not-exist "~/.emacs.d/personal.el")
 (load "~/.emacs.d/personal.el")
